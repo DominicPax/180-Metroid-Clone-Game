51b1a36938cde5996e51d189c045802256ca7fb6
Edward Resendiz <ed95969@gmail.com>
2025-10-30 11:05:49 -0700

    movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Resendiz Edward
+ * 10/30/25
+ * Handles Behaviors of the Player
+ */
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 direction;
+    private Rigidbody rb;
+
+    public float speed = 10f;
+    public float jumpForce = 10f;
+    public float groundCheckDist = 1.2f;
+    public float deathHeight = -3f;
+    public int lives = 3;
+
+    
+
+    private Vector3 respawnPos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        respawnPos = transform.position;
+    }
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Jump();
+        CheckForFallDeath();
+    }
+
+    //Called every 0.02
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void CheckForFallDeath()
+    {
+        if (transform.position.y < deathHeight)
+            Respawn();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        bool isGrounded = false;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDist))
+        {
+            isGrounded = true;
+        }
+
+        return isGrounded;
+    }
+
+    private void MovePlayer()
+    {        
+       
+        //Get input to move Left
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            
+            direction = Vector3.left;
+            //transform.position += direction * speed * Time.deltaTime;
+            rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+            transform.Rotate(0, 180, 0);
+        }
+
+        //Get input to move right
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector3.right;
+            //transform.position += direction * speed * Time.deltaTime;
+            rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+            transform.Rotate(0, -180, 0);
+        }
+        
+    }
+
+    public void Respawn()
+    {
+        transform.position = respawnPos;
+        lives--;
+
+        if(lives <= 0)
+        {
+            print("GAME OVER");
+        }
+    }
+
+}
