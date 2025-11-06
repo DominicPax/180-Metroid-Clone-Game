da54e332a63866900cfe7bee0ea1639de181b4ae
Edward Resendiz <ed95969@gmail.com>
2025-11-06 10:58:27 -0800

    fix health pack

--- Assets/Assets/CScripts/HealthPack.cs
+++ Assets/Assets/CScripts/HealthPack.cs
@@ -12,21 +12,20 @@ public class HealthPack : MonoBehaviour
 {
     public int healthpackValue = 50;
     public float rotateSpeed = 10f;
-    private float xValue = 50;
+
 
     // Update is called once per frame
     void Update()
     {
         //Spin coin on the Y axis
         transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
-        float xPos = Mathf.Clamp(xValue, 0, 99);
-    }
+    }    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
         {
-            other.GetComponent<PlayerController>().health += healthpackValue;
+            other.GetComponent<PlayerController>().AddHealth(healthpackValue);
             Destroy(gameObject);
         }
     }
